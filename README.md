7526a9fecae95f205a6ef90f7f409b089fae70e0
Dimas Tri Raharjo <dimastriraharjo24@gmail.com>
2023-02-23 17:16:43 +0700

    roles and permission with spatie

--- README.md
+++ README.md
@@ -17,3 +17,33 @@ Bisa aja content belajar dari playlist lain akan ku taruh sini juga.
 ### Master / Main / Init / Default / Starter
 
 -   Ini branch starter untuk memulai branch baru lainnya. Jadi disini ngak ada apa" ya cuma akan ada update di md ini doang
+
+### Laravel Spatie
+
+-   Spatie Permission: Multiple Roles Persmission & Middleware.
+-   Dokumentasi nya ada disini [Spatie: Laravel Permission](https://spatie.be/docs/laravel-permission/v5/introduction).
+-   Instalasi nya diikuti aja sampe beres.
+    -   composer require
+    -   publish vendor (copy file config & migration)
+    -   clear cache
+    -   migrate
+    -   use HasRoles di model User
+-   Spatie ini membantu kita dalam membuat role dan permission kepada user. Secara konsep sama kyk yg lain, akan dibikinin beberapa table dan pivot table untuk store role dan permission milik user, lalu akan ada middleware yg akan membatasi hak akses user tergantung role dan atau permission nya.
+-   Table yang dibikinin ada roles, permissions, model_has_roles, model_has_permissions, role_has_permissions.
+-   Inser data ke table manually, sebenarnya kalo mau pake seeder juga bisa dan lebih cepet mungkin biar beres bikinnya.
+
+```
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES (NULL, 'tony', 'tony@email.com', NULL, '$2y$10$GIm.Rya7cjacqEZDyF25tuLkw36eDMp7tqqB4g9lsRQ0LhYWUbZk2', NULL, NULL, NULL), (NULL, 'andy', 'andy@email.com', NULL, '$2y$10$GIm.Rya7cjacqEZDyF25tuLkw36eDMp7tqqB4g9lsRQ0LhYWUbZk2', NULL, NULL, NULL), (NULL, 'ben', 'ben@email.com', NULL, '$2y$10$GIm.Rya7cjacqEZDyF25tuLkw36eDMp7tqqB4g9lsRQ0LhYWUbZk2', NULL, NULL, NULL);
+```
+
+```
+INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES (NULL, 'author', 'web', NULL, NULL), (NULL, 'editor', 'web', NULL, NULL), (NULL, 'moderator', 'web', NULL, NULL);
+```
+
+```
+INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES (NULL, 'create article', 'web', NULL, NULL), (NULL, 'edit article', 'web', NULL, NULL), (NULL, 'delete article', 'web', NULL, NULL);
+```
+
+-   Kita assignment manual juga untuk role permission dan user, biar cepet langsung bikin function di route.
+-   Pendekatan penggunaan role dan permission ada dua umumnya. Pertama user has role has permission. Kedua user has permission. Ini tergantung kebutuhan aplikasi. Paling enak atau best practicenya yang ke pertama. Sebenernya ada yg ketiga jadi cuma user has role tapi tidak dianjurkan.
+-   Cara batasi akses dgn middleware default atau package, caranya liat langsung ke route tambahan pergi ke dokumentasi nya.
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Permission;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +18,109 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    // dd(Auth::user());
     return view('welcome');
 });
+
+Route::get('give-permission-to-role', function () {
+    // $role = Role::findOrFail(1); // author
+    // $permission = Permission::findOrFail(1); // create article
+
+    // $role = Role::findOrFail(2); // editor
+    // $permission = Permission::findOrFail(2); // edit article
+
+    // $role->givePermissionTo($permission);
+
+    // $role = Role::findOrFail(3); // editor
+    // $permission1 = Permission::findOrFail(1); // edit article
+    // $permission2 = Permission::findOrFail(2); // edit article
+    // $permission3 = Permission::findOrFail(3); // edit article
+
+    // $role->givePermissionTo([$permission1, $permission2, $permission3]);
+});
+
+Route::get('assign-role-to-user', function () {
+    // $user = User::findOrFail(1); // tony
+    // $role = Role::findOrFail(1); // author
+
+    // $user = User::findOrFail(2); // andy
+    // $role = Role::findOrFail(2); // editor
+
+    // $user = User::findOrFail(3); // ben
+    // $role = Role::findOrFail(3); // moderator
+
+    // $user->assignRole($role);
+
+    // $user = User::create([
+    //     'name' => 'jamal',
+    //     'email' => 'jamal@email.com',
+    //     'password' => '$2y$10$GIm.Rya7cjacqEZDyF25tuLkw36eDMp7tqqB4g9lsRQ0LhYWUbZk2',
+    // ]);
+    // $role1 = Role::findOrFail(1); // author
+    // $role2 = Role::findOrFail(2); // editor
+    // $role3 = Role::findOrFail(3); // moderator
+
+    // $user->assignRole([$role1, $role2, $role3]);
+});
+
+Route::get('spatie-method', function () {
+    // $user = User::findOrFail(1); // tony
+    // $user = User::findOrFail(4); // jamal
+
+    // dd($user->getPermissionNames()); // collection of name strings | gk ada krn bkn direct permission
+    // dd($user->permissions); // collection of permission objects | gk ada krn bkn direct permission
+
+    // dd($user->getDirectPermissions()); // sama kyk yg atas gk ada krn bkn direct
+    // dd($user->getPermissionsViaRoles()); // get permission lewat role
+    // dd($user->getAllPermissions()); // get semua permission (inherited or directly)
+
+    // dd($user->getRoleNames()); // Returns a collection
+
+    // $users = User::role('author')->get(); // Returns only users with the role 'author'
+    // $users = User::permission('edit article')->get(); // Returns only users with the permission 'edit article' (inherited or directly)
+    // dd($users);
+});
+
+// $user = User::findOrFail(1); // tony = author
+// $user = User::findOrFail(2); // andy = editor
+// $user = User::findOrFail(3); // ben = moderator
+// Auth::login($user);
+Auth::logout();
+
+// Route::get('create-article', function () {
+//     dd('ini create-article hanya bisa di akses oleh author dan moderator');
+// })->middleware('can:create article');
+
+// Route::get('edit-article', function () {
+//     dd('ini edit-article hanya bisa di akses oleh editor dan moderator');
+// })->middleware('can:edit article');
+
+// Route::get('delete-article', function () {
+//     dd('ini delete-article hanya bisa di akses oleh moderator');
+// })->middleware('can:delete article');
+
+// Route::get('create-article', function () {
+//     dd('ini create-article hanya bisa di akses oleh author dan moderator');
+// })->middleware('role:author|moderator');
+
+// Route::get('edit-article', function () {
+//     dd('ini edit-article hanya bisa di akses oleh editor dan moderator');
+// })->middleware('role:editor|moderator');
+
+// Route::get('delete-article', function () {
+//     dd('ini delete-article hanya bisa di akses oleh moderator');
+// })->middleware('role:moderator');
+
+Route::get('create-article', function () {
+    // Auth::logout();
+    // dd(Auth::user());
+    dd('ini create-article hanya bisa di akses oleh author dan moderator');
+})->middleware('permission:create article');
+
+Route::get('edit-article', function () {
+    dd('ini edit-article hanya bisa di akses oleh editor dan moderator');
+})->middleware('permission:edit article');
+
+Route::get('delete-article', function () {
+    dd('ini delete-article hanya bisa di akses oleh moderator');
+})->middleware('permission:delete article');
